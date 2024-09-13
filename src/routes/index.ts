2f9d784961d3b1b87b3cbf13e859ff39cb8efb2f
Weslley Rodrigues <weslley.rdgs@gmail.com>
2024-09-12 22:26:33 -0300

    fix: rendirecionamento de rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,9 @@ import * as SearchController from '../controllers/searchController';
 const route = Router();
 
 route.get('/', PageController.home);
-route.get('/dogs', PageController.home);
-route.get('/cats', PageController.home);
-route.get('/fishes', PageController.home);
+route.get('/dogs', PageController.dogs);
+route.get('/cats', PageController.cats);
+route.get('/fishes', PageController.fishes);
 
 route.get('/search', SearchController.search);
 
