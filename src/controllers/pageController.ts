b877cee69e167982c0f26dfcfba013bda8b8114b
Weslley Rodrigues <weslley.rdgs@gmail.com>
2024-10-09 09:19:30 -0300

    feat: ajusta controllers para retornar pets por tipo

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -4,18 +4,19 @@ import { createMenuObject } from '../helpers/menuObject'
 import { Pet } from '../models/pets';
 
 export const home = (req: Request, res: Response) => {
+  let pets = Pet.getAll();
   res.render('pages/page', {
     menu: createMenuObject(''),
     banner: {
       title: 'Todos os animais',
       background: 'allanimals.jpg'
-    }
+    },
+    pets
   })
 }
 
 export const dogs = (req: Request, res: Response)  => {
-  let pets = Pet.getAll();
-
+  let pets = Pet.getFromType('dog');
   res.render('pages/page', {
     menu: createMenuObject('dog'),
     banner: {
@@ -27,21 +28,25 @@ export const dogs = (req: Request, res: Response)  => {
 }
 
 export const cats = (req: Request, res: Response) => {
+  let pets = Pet.getFromType('cat');
   res.render('pages/page', {
     menu: createMenuObject('cat'),
     banner: {
       title: 'Todos os gatos',
       background: 'banner_cat.jpg'
-    }
+    },
+    pets
   })
 }
 
 export const fishes = (req: Request, res: Response)  => {
+  let pets = Pet.getFromType('fish');
   res.render('pages/page', {
     menu: createMenuObject('fish'),
     banner: {
       title: 'Todos os peixes',
       background: 'banner_fish.jpg'
-    }
+    },
+    pets
   })
 }
